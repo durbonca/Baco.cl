c0ae9626e6df1dba49512cfb599b334fc43c9c77
durbonca <durbonca@hotmail.com>
2020-03-25 15:50:20 -0300

    download button add

--- script/script.js
+++ script/script.js
@@ -1,44 +1,17 @@
-var date =
-    var carta = nombre
-
-var fileName = carta + date
-
 var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1500,
     speedAsDuration: true
 });
 
-/* Helper function */
-function download_file(fileURL, fileName) {
-    // for non-IE
-    if (!window.ActiveXObject) {
-        var save = document.createElement("a");
-        save.href = fileURL;
-        save.target = "_blank";
-        var filename = fileURL.substring(fileURL.lastIndexOf("/") + 1);
-        save.download = fileName || filename;
-        if (
-            navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) &&
-            navigator.userAgent.search("Chrome") < 0
-        ) {
-            document.location = save.href;
-            // window event not working here
-        } else {
-            var evt = new MouseEvent("click", {
-                view: window,
-                bubbles: true,
-                cancelable: false
-            });
-            save.dispatchEvent(evt);
-            (window.URL || window.webkitURL).revokeObjectURL(save.href);
-        }
-    }
+var d = new Date(),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-    // for IE < 11
-    else if (!!window.ActiveXObject && document.execCommand) {
-        var _window = window.open(fileURL, "_blank");
-        _window.document.close();
-        _window.document.execCommand("SaveAs", true, fileName || fileURL);
-        _window.close();
-    }
-}
+var fecha = [day, month, year].join("-");
+var carta = "carta";
+var fileName = carta + "-" + fecha + ".pdf";
+
+var fileURL = "./pdf/MENUD.pdf";
+
+var menuForm = document.getElementById("menuForm");
