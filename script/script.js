96065a2261b2b5b87e5119ed47ebed9727e09cb2
durbonca <durbonca@hotmail.com>
2020-03-27 11:29:08 -0300

    last finished changes... i hope, and adding fonts awesome

--- script/script.js
+++ script/script.js
@@ -2,16 +2,3 @@ var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1500,
     speedAsDuration: true
 });
-
-var d = new Date(),
-    month = "" + (d.getMonth() + 1),
-    day = "" + d.getDate(),
-    year = d.getFullYear();
-
-var fecha = [day, month, year].join("-");
-var carta = "carta";
-var fileName = carta + "-" + fecha + ".pdf";
-
-var fileURL = "./pdf/MENUD.pdf";
-
-var menuForm = document.getElementById("menuForm");
