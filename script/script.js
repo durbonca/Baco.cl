0d719f979440bba0eae80e870e2785704ad0808d
durbonca <durbonca@hotmail.com>
2020-03-28 16:38:06 -0300

    cambios realizados al 28mar2020

--- script/script.js
+++ script/script.js
@@ -1,4 +1,12 @@
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1500,
-    speedAsDuration: true
+var clipboard = new ClipboardJS('.btn');
+
+clipboard.on('success', function(e) {
+    console.log(e);
 });
+
+clipboard.on('error', function(e) {
+    console.log(e);
+});
+
+
+$('[data-toggle="tooltip-click"]').tooltip({ title: "Copiar", trigger: "hover", delay: { show: 100, hide: 500 } });
